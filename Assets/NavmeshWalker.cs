3e68a173939199cf3fcbbf4819fc77f91fa5a0d8
Thiago Hirata <hirata@devcase.com.br>
2016-08-25 14:08:17 -0300

    Demonstração de NavMesh

---
+++ Assets/NavmeshWalker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavmeshWalker : MonoBehaviour {
+    public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if(goal != null)
+        {
+            agent.destination = goal.transform.position;
+        }
+    }
+}
