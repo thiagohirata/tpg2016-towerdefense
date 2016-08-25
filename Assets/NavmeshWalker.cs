797030f1470083ca1acaefb5edf8b1af33ade8c7
Thiago Hirata <hirata@devcase.com.br>
2016-08-25 14:23:46 -0300

    NavmeshWalker - atualização de caminho apenas se destino mudar muito

--- Assets/NavmeshWalker.cs
+++ Assets/NavmeshWalker.cs
@@ -4,18 +4,29 @@ using System.Collections;
 [RequireComponent(typeof(NavMeshAgent))]
 public class NavmeshWalker : MonoBehaviour {
     public Transform goal;
+	private Vector3 destination;
+	private NavMeshAgent agent;
 
 	// Use this for initialization
 	void Start () {
-        
+		agent = GetComponent<NavMeshAgent>();
+		if(goal != null)
+        {
+			destination = goal.position;
+            agent.destination = destination;
+        }
     }
-	
+
 	// Update is called once per frame
 	void Update () {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
         if(goal != null)
         {
-            agent.destination = goal.transform.position;
+			// Update destination if the target moves one unit
+			if (Vector3.Distance (destination, goal.position) > 1.0f) {
+				destination = goal.position;
+				agent.destination = destination;
+			}
         }
     }
 }
