ef0062e76a1b1f2377389c65805cedc2c9145011
THIAGO MASSAO KURAOKA HIRATA <thiago.mkhirata1@senacsp.edu.br>
2016-08-27 12:17:30 -0300

    Gerador de inimigos

---
+++ Assets/GeradorDeInimigosBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeradorDeInimigosBehaviour : MonoBehaviour {
+
+    public Transform prefab;
+    public int periodo = 10;
+    public Transform goal;
+    private float ultimoInimigoGerado = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       if (Time.time - ultimoInimigoGerado > periodo) {
+            Transform inimigoCriado = Instantiate(prefab);
+            ultimoInimigoGerado = Time.time;
+            inimigoCriado.GetComponent<NavmeshWalker>().goal = this.goal;
+
+       }
+    }
+}
