0fad2453e1b40aa77e93b33cffbeddc8e9ea335c
THIAGO MASSAO KURAOKA HIRATA <thiago.mkhirata1@senacsp.edu.br>
2016-08-27 10:34:35 -0300

    segue o mouse

---
+++ Assets/MoveToMouseBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToMouseBehaviour : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            
+            Transform objectHit = hit.transform;
+            this.transform.position = hit.point;
+            // Do something with the object that was hit by the raycast.
+        }
+
+    }
+}
